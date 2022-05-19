103e430713d8efd6766d21d83b10a3427fdc9c30
guojunda <2675814363@qq.com>
2022-05-19 13:50:23 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+﻿
+
+$('#login-button').click(function (event) {
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(pwd=="123"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="firstPage.html";},2000);
+		}
+	else{
+		alert("密码错误");
+		return
+	}
+});
